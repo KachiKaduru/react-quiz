62323297e97efb7111821e38daffea06317ed05e
Kachii <kachikaduru@gmail.com>
2024-07-15 23:07:12 +0100

    creating the usereducer hook and functions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import Main from "./components/Main";
 import Header from "./Header";
 
+const initialState = {};
+
+function reducer() {}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
       .then((data) => console.log(data))
-      .catch((err) => console.error("error"));
+      .catch((err) => console.error("error", err));
   }, []);
 
   return (
